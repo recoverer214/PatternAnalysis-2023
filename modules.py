030830c33c8af4376604ffafc286a7340a3f2168
recoverer214 <recoverer214.com>
2024-12-22 22:47:29 +1000

    UNet structure is complete.

---
+++ modules.py
@@ -0,0 +1,103 @@
+import torch
+from torch import nn
+
+class DoubleConv(nn.Module):
+    """DoubleConv is a basic building block of the encoder and decoder components. 
+    Consists of two convolutional layers followed by a ReLU activation function.
+    """    
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        x = self.double_conv(x)
+        return x
+
+
+class Down(nn.Module):
+    """Downscaling.
+    Consists of two consecutive DoubleConv blocks followed by a max pooling operation.
+    """    
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        x = self.maxpool_conv(x)
+        return x
+
+
+class Up(nn.Module):
+    """Upscaling.
+    Performed using transposed convolution and concatenation of feature maps from the corresponding "Down" operation.
+    """
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+
+        # input tensor shape: (batch_size, channels, height, width)
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = nn.functional.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+    
+
+class UNet(nn.Module):
+    def __init__(self, num_channels=1, num_classes = 6, bilinear=False):
+        '''
+        Construct a typical 2D UNet.
+        Parameters:
+        num_channels: Number of factors to represent colors. 1 = greyscale, 3 = RGB
+        num_classes: The number of classes. Each layer represents the probability of pixels
+        falling into corresponding class.
+        '''
+        super(UNet, self).__init__()
+        self.inc = (DoubleConv(num_channels, 64))
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        
+        self.down4 = Down(512,1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = nn.Conv2d(64, num_classes, 1)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
+        x = torch.sigmoid(x) #The probability of each class per pixel
+        return x
